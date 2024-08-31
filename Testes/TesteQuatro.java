733d74e7dcd3b00f5b6892aacb9ef093f1025805
Gabfonexe <bielfrancisco23@gmail.com~>
2024-08-31 14:01:10 -0300

    Teste 4

---
+++ Testes/TesteQuatro.java
@@ -0,0 +1,27 @@
+package Testes;
+
+public class TesteQuatro {
+
+  // Descubra a lógica e complete o próximo elemento:
+  // a) 1, 3, 5, 7, ___
+  // b) 2, 4, 8, 16, 32, 64, ____
+  // c) 0, 1, 4, 9, 16, 25, 36, ____
+  // d) 4, 16, 36, 64, ____
+  // e) 1, 1, 2, 3, 5, 8, ____
+  // f) 2,10, 12, 16, 17, 18, 19, ____
+  
+  public static void main(String[] args) {
+
+    System.out.println("Lógica de cada Item: \n\n a) a razão é a soma em 2 unidades a cada número \n b) a razão é o dobro de cada número \n c) a razão coemça como 1 e depois é acrescido +2 em cada razão seguinte, ou seja, ela é mutável \n d) a razão começa como 12 e depois é acrescido 8 unidades em cada nova razão \n e) a razão é uma sequência de Fibonacci, onde soma-se os 2 números anteriores ao novo número. Única diferença é que não se iniciou com 0 \n f) a razão é que todos os números começam com a letra [D], por isso o próximo é [Duzentos] \n" );
+
+    System.out.println("Resultado: ");
+    System.out.println("a) 9");
+    System.out.println("b) 128");
+    System.out.println("c) 49");
+    System.out.println("d) 100");
+    System.out.println("e) 13");
+    System.out.println("f) 200");
+
+  }
+  
+}
